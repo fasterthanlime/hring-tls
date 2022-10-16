b2a5425e3ed5e4258a22b79db3796e24f759e94a
Amos Wenger <amoswenger@gmail.com>
2022-10-16 21:17:05 +0200

    use hring to proxy to httpbingo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::{
+    net::ToSocketAddrs,
     os::unix::prelude::{AsRawFd, FromRawFd},
     rc::Rc,
     sync::Arc,
 };
 
 use color_eyre::eyre;
-use hring::tokio_uring::net::TcpStream;
+use hring::{
+    bufpool::AggBuf,
+    tokio_uring::net::TcpStream,
+    types::{ConnectionDriver, RequestDriver},
+};
 use rustls::ServerConfig;
 use tokio::net::TcpListener;
 use tracing::info;
@@ -38,14 +43,17 @@ async fn async_main() -> eyre::Result<()> {
     let acceptor = tokio_rustls::TlsAcceptor::from(Arc::new(server_config));
     let acceptor = Rc::new(acceptor);
 
+    let conn_dv = Rc::new(ConnDv {});
+
     let ln = TcpListener::bind("[::]:7000").await?;
     info!("Listening on {}", ln.local_addr()?);
 
     while let Ok((stream, remote_addr)) = ln.accept().await {
         hring::tokio_uring::spawn({
             let acceptor = acceptor.clone();
+            let conn_dv = conn_dv.clone();
             async move {
-                if let Err(e) = handle_conn(acceptor, stream, remote_addr).await {
+                if let Err(e) = handle_conn(acceptor, conn_dv, stream, remote_addr).await {
                     tracing::error!(%e, "Error handling connection");
                 }
             }
@@ -57,31 +65,53 @@ async fn async_main() -> eyre::Result<()> {
 
 async fn handle_conn(
     acceptor: Rc<tokio_rustls::TlsAcceptor>,
+    conn_dv: Rc<ConnDv>,
     stream: tokio::net::TcpStream,
     remote_addr: std::net::SocketAddr,
 ) -> Result<(), color_eyre::Report> {
     info!("Accepted connection from {remote_addr}");
     let stream = acceptor.accept(stream).await?;
+
     info!("Performed TLS handshake");
     let stream = ktls::config_ktls_server(stream)?;
+
     info!("Set up kTLS");
     let (drained, stream) = stream.into_raw();
     let drained = drained.unwrap_or_default();
-    info!("Drained {} bytes", drained.len());
-    assert_eq!(drained.len(), 0);
+    info!("{} bytes already decoded by rustls", drained.len());
 
     let fd = stream.as_raw_fd();
     std::mem::forget(stream);
     let stream = unsafe { TcpStream::from_raw_fd(fd) };
-    let body = "Hello from hring-tls\n";
-    let content_length = body.len();
-    let (res, _) = stream
-        .write_all(
-            format!("HTTP/1.1 200 OK\r\ncontent-length: {content_length}\r\n\r\n{body}")
-                .into_bytes(),
-        )
-        .await;
-    res?;
+
+    let buf = AggBuf::default();
+    buf.write().put(&drained[..])?;
+    hring::proto::h1::proxy(conn_dv, stream, buf).await?;
 
     Ok(())
+}
+
+struct ConnDv {}
+
+impl ConnectionDriver for ConnDv {
+    type RequestDriver = ReqDv;
+
+    fn steer_request(&self, _req: &hring::types::Request) -> eyre::Result<Self::RequestDriver> {
+        Ok(ReqDv {})
+    }
+}
+
+struct ReqDv {}
+
+impl RequestDriver for ReqDv {
+    fn upstream_addr(&self) -> eyre::Result<std::net::SocketAddr> {
+        Ok("httpbingo.org:80"
+            .to_socket_addrs()?
+            .next()
+            .expect("http bingo should be up"))
+    }
+
+    fn keep_header(&self, _name: &str) -> bool {
+        true
+    }
 }
